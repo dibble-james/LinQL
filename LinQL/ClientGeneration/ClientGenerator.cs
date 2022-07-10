72f78fbb03597e7192dc44086daa5d6f4af1679e
James Dibble <dibble-james@github.com>
2022-07-10 11:28:25 +0100

    Add custom namespace/name attributes

--- LinQL/ClientGeneration/ClientGenerator.cs
+++ LinQL/ClientGeneration/ClientGenerator.cs
@@ -10,12 +10,28 @@ using Microsoft.CodeAnalysis.Text;
 [Generator]
 public class ClientGenerator : ISourceGenerator
 {
-    private static readonly DiagnosticDescriptor InvalidXmlWarning = new(
+    private static readonly DiagnosticDescriptor StartingInfo = new(
         id: "LINQLGEN01",
         title: "Starting code generation",
         messageFormat: "Starting code generation for file {0}",
         category: "LinQLClientGenerator",
         defaultSeverity: DiagnosticSeverity.Info,
+        isEnabledByDefault: true);
+
+    private static readonly DiagnosticDescriptor MissingNamespace = new(
+        id: "LINQLGEN02",
+        title: "Missing namespace attribute",
+        messageFormat: "GraphQL file {0} does not have a LinQLClientNamespace attribute. Please add it to the AdditionalFiles element in your CSProj file.",
+        category: "LinQLClientGenerator",
+        defaultSeverity: DiagnosticSeverity.Error,
+        isEnabledByDefault: true);
+
+    private static readonly DiagnosticDescriptor MissingClientName = new(
+        id: "LINQLGEN03",
+        title: "Missing namespace attribute",
+        messageFormat: "GraphQL file {0} does not have a LinQLClientName attribute. Please add it to the AdditionalFiles element in your CSProj file.",
+        category: "LinQLClientGenerator",
+        defaultSeverity: DiagnosticSeverity.Error,
         isEnabledByDefault: true);
 
     /// <summary>
@@ -39,17 +55,32 @@ public class ClientGenerator : ISourceGenerator
     /// <inheritdoc/>
     public void Execute(GeneratorExecutionContext context)
     {
-
         foreach (var schema in context.AdditionalFiles.Where(x => x.Path.EndsWith(".graphql")))
         {
-            context.ReportDiagnostic(Diagnostic.Create(InvalidXmlWarning, Location.None, schema.Path));
+            context.ReportDiagnostic(Diagnostic.Create(StartingInfo, Location.None, schema.Path));
+
+            if (!context.AnalyzerConfigOptions.GetOptions(schema).TryGetValue("build_metadata.additionalfiles.LinQLClientNamespace", out var ns))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(MissingNamespace, Location.None, schema.Path));
+            }
+
+            if (!context.AnalyzerConfigOptions.GetOptions(schema).TryGetValue("build_metadata.additionalfiles.LinQLClientName", out var clientName))
+            {
+                context.ReportDiagnostic(Diagnostic.Create(MissingClientName, Location.None, schema.Path));
+            }
+
+            if (ns is null || clientName is null)
+            {
+                continue;
+            }
+
             var content = schema.GetText(context.CancellationToken)!.ToString();
             var schemaName = Path.GetFileName(schema.Path).Replace(".graphql", string.Empty);
 
             context.AddSource(
                 $"{schemaName}.g.cs",
                 SourceText.From(
-                    Generate(content, schemaName, schemaName + ".Client"),
+                    Generate(content, clientName, ns),
                     Encoding.UTF8)
                 );
         }
